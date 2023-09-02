12c8dfdeef6005c9aa8f1aa3e98b8ffce89412ff
Thomas Berthaud <tomberthaud@gmail.com>
2023-09-02 12:28:25 +0200

    feature/mg-64 : add fontawesome icon library

--- src/lib/ui/Home.svelte
+++ src/lib/ui/Home.svelte
@@ -4,7 +4,10 @@
 	export let user: User;
 </script>
 
-<h1 class="pt-3 text-center h1">Bienvenue {user.user_metadata.user_name}</h1>
+<header class="flex items-center justify-center gap-5">
+	<h1 class="h1">Bienvenue {user.user_metadata.user_name}</h1>
+	<i class="fa-regular fa-user fa-2x"></i>
+</header>
 <section class="mt-8 px-12 flex flex-col justify-center">
 	<a href="/games/create" class="btn btn-xl variant-filled-primary"> Jouer </a>
 	<div class="py-4 self-center">OU</div>
