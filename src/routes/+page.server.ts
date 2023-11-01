37aed625d8395490163dcaeb6dfc3339cf1204b5
Thomas Berthaud <tomberthaud@gmail.com>
2023-11-01 19:37:55 +0100

    feature/mg-65: redirect to login when user is not logged in

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,35 +1,13 @@
-import { fail } from '@sveltejs/kit';
-import type { Actions, LayoutServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
 	const session = await getSession();
 	if (!session) {
-		return {};
+		throw redirect(307, '/login');
 	}
 
 	return {
 		user: session.user
 	};
-};
-
-export const actions: Actions = {
-	login: async ({ request, cookies, locals }) => {
-		const data = await request.formData();
-
-		const userName = data.get('user-name');
-
-		if (!userName) {
-			return fail(400, { missingUserName: true });
-		}
-
-		const userId = cookies.get('uuid');
-		const response = await locals.authService.authenticateUser(userId, userName.toString());
-
-		if (response.error) {
-			console.error(response.error);
-			return fail(400, { unknownError: true });
-		}
-
-		cookies.set('uuid', response.data.userId);
-	}
 };
