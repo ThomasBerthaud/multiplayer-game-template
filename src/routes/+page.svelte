b5b495eb2866ed2634c71c548e3b788453cb8d2b
Thomas Berthaud <tomberthaud@gmail.com>
2023-11-01 19:37:55 +0100

    feature/mg-65: redirect to login when user is not logged in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-	import UserNameForm from '$lib/ui/Users/UserNameForm.svelte';
-	import Home from '$lib/ui/Home.svelte';
-	import type { ActionData, PageData } from './$types';
+	import type { PageData } from './$types';
 
-	export let form: ActionData;
 	export let data: PageData;
 
 	$: ({ user } = data);
 </script>
 
-<!--TODO : faire plutot un guard au lieu d'un if/else-->
-{#if user}
-	<Home {user} />
-{:else}
-	<!--TODO : prerender login form-->
-	<UserNameForm {form} />
-{/if}
+<header class="flex items-center justify-center gap-5">
+	<h1 class="h1">Bienvenue {user.user_metadata.user_name}</h1>
+	<i class="fa-regular fa-user fa-2x"></i>
+</header>
+<section class="mt-8 px-12 flex flex-col justify-center">
+	<a href="/games/create" class="btn btn-xl variant-filled-primary"> Jouer </a>
+	<div class="py-4 self-center">OU</div>
+	<a href="/games/join" class="btn btn-xl variant-ringed-secondary"> Rejoindre une partie </a>
+</section>
---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,25 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
+export const actions: Actions = {
+	login: async ({ request, cookies, locals }) => {
+		const data = await request.formData();
+
+		const userName = data.get('user-name');
+
+		if (!userName) {
+			return fail(400, { missingUserName: true });
+		}
+
+		const userId = cookies.get('uuid');
+		const response = await locals.authService.authenticateUser(userId, userName.toString());
+
+		if (response.error) {
+			console.error(response.error);
+			return fail(400, { unknownError: true });
+		}
+
+		cookies.set('uuid', response.data.userId);
+		throw redirect(303, '/');
+	}
+};
