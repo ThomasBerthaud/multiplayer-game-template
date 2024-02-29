1aa00897b6c669670068887b2bbfbdafc9c7759f
Thomas Berthaud <tomberthaud@gmail.com>
2024-02-29 23:09:28 +0100

    feature/mg-101: Homepage UI

--- app/routes/_index/JoinGameForm.tsx
+++ app/routes/_index/JoinGameForm.tsx
@@ -1,22 +1,29 @@
 import { useFetcher } from '@remix-run/react';
 import { useZorm } from 'react-zorm';
 import { GameJoinFormSchema } from '~/domain/Games/schemas/GameJoinForm.schema';
+import { Input, InputGroup, InputRightElement } from '@chakra-ui/input';
+import { Box, FormControl, FormErrorMessage, IconButton } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
 
 export default function JoinGameForm() {
     const fetcher = useFetcher();
     const zo = useZorm('GameJoin', GameJoinFormSchema);
 
     return (
-        <fetcher.Form ref={zo.ref} method="post" action="/games/join">
-            <label htmlFor={zo.fields.gameCode()} className="label">
-                <span>Identifiant de la partie</span>
-                <input className="input" type="text" id="game-code" name={zo.fields.gameCode()} />
-            </label>
-            <div className="text-center">
-                <button type="submit" className="btn btn-xl variant-filled-primary my-2">
-                    Rejoindre
-                </button>
-            </div>
-        </fetcher.Form>
+        <Box w="60%" mt={2} mb={8}>
+            <fetcher.Form ref={zo.ref} method="post" action="/games/join">
+                <FormControl isInvalid={!!zo.errors.gameCode()}>
+                    <InputGroup size="lg">
+                        <Input pr="4.5rem" type="text" name={zo.fields.gameCode()} placeholder="Rejoindre une partie" />
+                        <InputRightElement>
+                            <IconButton type="submit" aria-label="Rejoindre" icon={<ArrowForwardIcon />} />
+                        </InputRightElement>
+                    </InputGroup>
+                    {zo.errors.gameCode((e) => (
+                        <FormErrorMessage>{e.message}</FormErrorMessage>
+                    ))}
+                </FormControl>
+            </fetcher.Form>
+        </Box>
     );
 }
