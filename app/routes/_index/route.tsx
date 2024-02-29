e769626d846f83f77e34eff79f0c3a4fb96216e5
Thomas Berthaud <tomberthaud@gmail.com>
2024-02-29 23:09:28 +0100

    feature/mg-101: Homepage UI

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,6 +1,8 @@
-import { useFetcher, useNavigation } from '@remix-run/react';
-import { AbsoluteCenter, Box, Button, Card, Divider, Heading, VStack } from '@chakra-ui/react';
+import { Link, useFetcher, useNavigation } from '@remix-run/react';
+import { Box, Button, Center, Container, Divider, Heading, HStack, useColorModeValue, VStack } from '@chakra-ui/react';
 import JoinGameForm from '~/routes/_index/JoinGameForm';
+import { AddIcon } from '@chakra-ui/icons';
+import ColorModeToggle from '~/components/ColorModeToggle';
 
 export default function Index() {
     const fetcher = useFetcher();
@@ -11,25 +13,37 @@ export default function Index() {
         fetcher.submit({}, { action: '/games/create', method: 'post' });
     };
 
+    const bgColor = useColorModeValue('gray.200', 'gray.700');
+
     return (
-        <VStack spacing={6}>
-            <Box w="100%" p={8} bgColor="purple.400">
-                <Heading textAlign="center">Bienvenue</Heading>
+        <VStack h="100vh">
+            <Box w="100%" p={8} bgColor={bgColor}>
+                <HStack justifyContent="space-between">
+                    <Heading textAlign="center">The Game</Heading>
+                    <ColorModeToggle />
+                </HStack>
             </Box>
-            <Card w="60%" alignItems="center" py={4} px={6}>
-                <VStack w="100%" spacing={4}>
-                    <Button type="button" onClick={onCreateGame} isLoading={isLoading}>
-                        Créer une partie
-                    </Button>
-                    <Box w="100%" my={5}>
-                        <Divider w="100%" />
-                        <AbsoluteCenter bg="white" px="4">
-                            OU
-                        </AbsoluteCenter>
-                    </Box>
-                    <JoinGameForm />
-                </VStack>
-            </Card>
+            <Container flex="1">
+                <Center minHeight="80%">
+                    <VStack w="100%">
+                        <Button
+                            w="60%"
+                            size="lg"
+                            type="button"
+                            onClick={onCreateGame}
+                            isLoading={isLoading}
+                            rightIcon={<AddIcon />}
+                        >
+                            Créer une partie
+                        </Button>
+                        <JoinGameForm />
+                        <Divider w="60%" />
+                        <Button mt={8} size="lg" as={Link} to="/about" variant="ghost">
+                            À propos
+                        </Button>
+                    </VStack>
+                </Center>
+            </Container>
         </VStack>
     );
 }
